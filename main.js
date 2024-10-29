6b035126f7cbeeaf031c797e555e3a8fb9403673
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-29 14:07:52 +0100

    1-names-complete

---
+++ main.js
@@ -0,0 +1,24 @@
+/* 
+Lavoriamo con i moduli commonjs esportando e importando valori e funzioni.
+Segui questi tre step creando i rispettivi files:
+1 - names.js
+Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+Esporta la funzione dal file.
+
+2 - hobbies.js
+Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire un oggetto con una proprietà hobbies, che è un array di hobby.
+Esporta la funzione dal file.
+
+3 - people.js*
+Importa la tua funzione da names.js
+Importa la tua funzione da hobbies.js
+Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con tre proprietà: firstName, lastName, hobbies.
+All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
+*/
+
+//1 - names.js
+const names = require("./names")
+console.log(names);
+
+
+
---
+++ names.js
@@ -0,0 +1,11 @@
+function full_name(first_name, last_name) {
+    let person = {
+        first_name: first_name,
+        last_name: last_name
+    }
+    return person
+}
+
+console.log(full_name("giovanni", "lumini"));
+
+module.exports = full_name;
